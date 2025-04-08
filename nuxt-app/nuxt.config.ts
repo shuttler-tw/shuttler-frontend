79e41a787b78430a84dcf1c8ff4c607da2ba2754
Vic <syuan0212@gmail.com>
2025-04-08 17:05:47 +0800

    nuxt init

---
+++ nuxt-app/nuxt.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@pinia/nuxt', '@element-plus/nuxt'],
+  css: ['~/assets/css/main.css'],
+  elementPlus: {
+    importStyle: "scss",
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/assets/css/styles/element/index.scss";`,
+        },
+      },
+    },
+  },
+})
---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const active = ref(0);
+const next = () => {
+  if (active.value++ > 2) active.value = 0
+}
+</script>
+
+<template>
+  <div class="text-center">
+    <h1 class="text-2xl mb-10">Welcome to Nuxt 3</h1>
+    <p class="text-primary">Nuxt 3 is a powerful framework for building Vue.js applications.</p>
+    <p class="text-warning">Explore the features and capabilities of Nuxt 3.</p>
+    <p class="text-danger">Get started by creating your first page!</p>
+    <el-button @click="ElMessage('hello')">button</el-button>
+    <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+  </div>
+</template>
+
+<style scoped></style>
