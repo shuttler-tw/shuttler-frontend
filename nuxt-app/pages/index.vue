a1eae1773a8340a346798edacfc3598c170843be
Vic <syuan0212@gmail.com>
2025-04-09 09:48:57 +0800

    feat: add pinia and counter store

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,8 +1,6 @@
 <script lang="ts" setup>
-const active = ref(0);
-const next = () => {
-  if (active.value++ > 2) active.value = 0
-}
+const useCounter = useCounterStore()
+const { increment, decrement, reset } = useCounter
 </script>
 
 <template>
@@ -11,8 +9,12 @@ const next = () => {
     <p class="text-primary">Nuxt 3 is a powerful framework for building Vue.js applications.</p>
     <p class="text-warning">Explore the features and capabilities of Nuxt 3.</p>
     <p class="text-danger">Get started by creating your first page!</p>
-    <el-button @click="ElMessage('hello')">button</el-button>
-    <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+    <p class="text-success font-bold">count:{{ useCounter.count }}</p>
+    <p class="text-info">doubleCount:{{ useCounter.doubleCount }}</p>
+    <el-button @click="ElMessage('hello')">message hello</el-button>
+    <ElButton :icon="ElIconPlus" type="success" @click="increment">increment</ElButton>
+    <ElButton :icon="ElIconMinus" type="danger" @click="decrement">decrement</ElButton>
+    <ElButton :icon="ElIconRefreshLeft" type="info" @click="reset">reset</ElButton>
   </div>
 </template>
 
