fdcd86eed3b4055389856b26fddf15b9c420f2cc
Vic <syuan0212@gmail.com>
2025-04-09 16:41:59 +0800

    chore: integrate Prettier with ESLint for consistent code formatting

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,20 +1,40 @@
 <script lang="ts" setup>
-const useCounter = useCounterStore()
-const { increment, decrement, reset } = useCounter
+  const useCounter = useCounterStore();
+  const { increment, decrement, reset } = useCounter;
 </script>
 
 <template>
   <div class="text-center">
     <h1 class="text-2xl mb-10">Welcome to Nuxt 3</h1>
-    <p class="text-primary">Nuxt 3 is a powerful framework for building Vue.js applications.</p>
+    <p class="text-primary">
+      Nuxt 3 is a powerful framework for building Vue.js applications.
+    </p>
     <p class="text-warning">Explore the features and capabilities of Nuxt 3.</p>
     <p class="text-danger">Get started by creating your first page!</p>
     <p class="text-success font-bold">count:{{ useCounter.count }}</p>
     <p class="text-info">doubleCount:{{ useCounter.doubleCount }}</p>
     <el-button @click="ElMessage('hello')">message hello</el-button>
-    <ElButton :icon="ElIconPlus" type="success" @click="increment">increment</ElButton>
-    <ElButton :icon="ElIconMinus" type="danger" @click="decrement">decrement</ElButton>
-    <ElButton :icon="ElIconRefreshLeft" type="info" @click="reset">reset</ElButton>
+    <ElButton
+      :icon="ElIconPlus"
+      type="success"
+      @click="increment"
+    >
+      increment
+    </ElButton>
+    <ElButton
+      :icon="ElIconMinus"
+      type="danger"
+      @click="decrement"
+    >
+      decrement
+    </ElButton>
+    <ElButton
+      :icon="ElIconRefreshLeft"
+      type="info"
+      @click="reset"
+    >
+      reset
+    </ElButton>
   </div>
 </template>
 
--- nuxt-app/stores/counter.ts
+++ nuxt-app/stores/counter.ts
@@ -1,14 +1,14 @@
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
+export const useCounterStore = defineStore("counter", () => {
+  const count = ref(0);
+  const doubleCount = computed(() => count.value * 2);
   function increment() {
-    count.value++
+    count.value++;
   }
   function decrement() {
-    count.value--
+    count.value--;
   }
   function reset() {
-    count.value = 0
+    count.value = 0;
   }
-  return { count, doubleCount, increment, decrement, reset }
-})
+  return { count, doubleCount, increment, decrement, reset };
+});
