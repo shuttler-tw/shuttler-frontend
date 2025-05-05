ab83f3339b9089b1f5ea01d29b3eda1d17126971
Vic <syuan0212@gmail.com>
2025-05-05 20:54:57 +0800

    fix: import Money icon component from Element Plus

--- nuxt-app/pages/member-center.vue
+++ nuxt-app/pages/member-center.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
   import ElMenuForUser from "~/components/memberCenter/ElMenuForUser.vue";
+  import { Money } from "@element-plus/icons-vue";
 
   const defaultAvator = ref(
     "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
