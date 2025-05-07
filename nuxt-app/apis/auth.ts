55bbbdb24170c1c254509176a075445aec099573
Chun-Chi Wang <limitsea0103@gmail.com>
2025-05-07 23:36:57 +0800

    feat: integrate sign in / sign up api

--- nuxt-app/apis/auth.ts
+++ nuxt-app/apis/auth.ts
@@ -8,3 +8,25 @@ export const getTestData = () => {
 export const testLogin = (loginData: { email: string; password: string }) => {
   return useShuttlerTwAPI.post("/testLogin", loginData, { watch: false });
 };
+
+export const emailLogin = (loginData: { email: string; password: string }) => {
+  return useShuttlerTwAPI.post<{
+    data: {
+      token: string;
+      user: {
+        id: string;
+        email: string;
+        name: string;
+      };
+    };
+    message: string;
+  }>("/auth/login", loginData, { watch: false });
+};
+
+export const emailSignUp = (signUpData: {
+  email: string;
+  password: string;
+  name: string;
+}) => {
+  return useShuttlerTwAPI.post("/auth/signup", signUpData, { watch: false });
+};
--- nuxt-app/components/LoginSignUpDialog.vue
+++ nuxt-app/components/LoginSignUpDialog.vue
@@ -1,24 +1,49 @@
 <script setup lang="ts">
-  defineProps({
-    visible: {
-      type: Boolean,
-      default: false
-    },
-    isSignUp: {
-      type: Boolean,
-      default: false
+import { emailLogin, emailSignUp } from "@/apis/auth";
+import { da } from "element-plus/es/locales.mjs";
+import { useAuthStore } from "~/stores/auth";
+
+const { visible, isSignUp } = defineProps({
+  visible: {
+    type: Boolean,
+    default: false,
+  },
+  isSignUp: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const form = ref({
+  email: "",
+  password: "",
+  name: "",
+});
+
+const emit = defineEmits<{
+  "update:visible": [value: boolean];
+}>();
+
+const authStore = useAuthStore();
+
+const handleSubmit = async () => {
+  if (isSignUp) {
+    await emailSignUp({
+      email: form.value.email,
+      password: form.value.password,
+      name: form.value.name,
+    });
+  } else {
+    const {data} = await emailLogin({
+      email: form.value.email,
+      password: form.value.password,
+    });
+
+    if (data.value?.data.token) {
+      authStore.setToken(data.value?.data.token);
     }
-  });
-
-  const form = ref({
-    email: "",
-    password: "",
-    displayName: ""
-  });
-
-  const emit = defineEmits<{
-    "update:visible": [value: boolean];
-  }>();
+  }
+};
 </script>
 
 <template>
@@ -32,41 +57,27 @@
   >
     <div class="text-center">尋找你心目中的羽球活動</div>
 
-    <div
-      v-if="!isSignUp"
-      class="flex flex-col items-center mt-5"
-    >
-      <el-form
-        label-position="top"
-        label-width="auto"
-        style="width: 60%"
-      >
+    <div v-if="!isSignUp" class="flex flex-col items-center mt-5">
+      <el-form label-position="top" label-width="auto" style="width: 60%">
         <el-form-item label="帳號">
           <el-input v-model="form.email" />
         </el-form-item>
         <el-form-item label="密碼">
-          <el-input v-model="form.password" />
+          <el-input v-model="form.password" type="password" />
         </el-form-item>
       </el-form>
     </div>
 
-    <div
-      v-else
-      class="flex flex-col items-center mt-5"
-    >
-      <el-form
-        label-position="top"
-        label-width="auto"
-        style="width: 60%"
-      >
+    <div v-else class="flex flex-col items-center mt-5">
+      <el-form label-position="top" label-width="auto" style="width: 60%">
         <el-form-item label="帳號">
           <el-input v-model="form.email" />
         </el-form-item>
         <el-form-item label="密碼">
-          <el-input v-model="form.password" />
+          <el-input v-model="form.password" type="password" />
         </el-form-item>
         <el-form-item label="暱稱">
-          <el-input v-model="form.displayName" />
+          <el-input v-model="form.name" />
         </el-form-item>
       </el-form>
     </div>
@@ -76,7 +87,7 @@
         <el-button
           type="primary"
           class="text-black w-[150px]"
-          @click="emit('update:visible', false)"
+          @click="handleSubmit"
         >
           {{ isSignUp ? "註冊" : "登入" }}
         </el-button>
