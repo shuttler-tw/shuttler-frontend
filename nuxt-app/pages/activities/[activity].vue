5d07033a0786d624b670c58cb97fab95d0612bd6
Vic <syuan0212@gmail.com>
2025-05-19 16:22:58 +0800

    feat: update activity pictures slicing

--- nuxt-app/pages/activities/[activity].vue
+++ nuxt-app/pages/activities/[activity].vue
@@ -1,26 +1,16 @@
 <script lang="ts" setup>
   import { getActivity } from "@/apis/activities";
+  import { activityPictures as defaultActivityPictures } from "~/constants/activityPictures";
+  import ActivityPictures from "~/components/activity/ActivityPictures.vue";
 
   const router = useRoute();
   const activityId = router.params.activity as string;
   const { data } = await getActivity(activityId);
   const activity = ref(data.value?.data);
-  const defaultPicture = ref([
-      "https://images.unsplash.com/photo-1729166241032-5b339506a0d7?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1599474924187-334a4ae5bd3c?q=80&w=2883&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1564769353575-73f33a36d84f?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1659081463572-4c5903a309e6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1733141731755-272381a17c59?q=80&w=2910&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    ]
-  );
-  const pictures = computed(() => {
-    return (activity.value?.pictures?.length ?? 0) > 0
-      ? activity.value?.pictures
-      : defaultPicture.value;
-  })
-  const pictureMaxHeight = {
-    default: "max-h-[428px]",
-    mobile: "max-h-[236px]"
+
+  const pictures = ref(defaultActivityPictures);
+  if (activity.value?.pictures && activity.value?.pictures.length > 0) {
+    pictures.value = activity.value?.pictures;
   }
 </script>
 <template>
@@ -36,38 +26,7 @@
       </el-breadcrumb>
     </div>
     <div class="container">
-      <div class="grid grid-cols-2 gap-6" :class="`${pictureMaxHeight.default}`">
-        <el-image
-          :src="(pictures ?? [])[0]"
-          :zoom-rate="1.2"
-          :max-scale="7"
-          :min-scale="0.2"
-          :preview-src-list="pictures"
-          show-progress
-          :initial-index="0"
-          fit="cover"
-          class="col-span-1"
-        />
-        <div class="grid grid-cols-2 gap-6" :class="`${pictureMaxHeight.default}`">
-          <el-image
-            v-for="(pictureUrl, index) in (pictures || []).slice(1, 5)"
-            :key="pictureUrl"
-            :src="pictureUrl"
-            :zoom-rate="1.2"
-            :max-scale="7"
-            :min-scale="0.2"
-            :preview-src-list="pictures"
-            show-progress
-            :initial-index="index + 1"
-            fit="cover"
-          />
-        </div>
-      </div>
+      <ActivityPictures :pictures="pictures" />
     </div>
   </div>
 </template>
-<style scoped lang="scss">
-  :deep(.el-image) img.el-image__inner.el-image__preview {
-    border-radius: var(--radius-2xl) !important;
-  }
-</style>
